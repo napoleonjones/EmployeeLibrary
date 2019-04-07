b5f1a75e80566ced0e18224e0c74f59556f6d9a2
Napoleon Jones <napoleon.jones@gotnext.io>
2019-04-07 01:45:43 -0400

    Added EmployeePayCalculators

---
+++ EmployeeLibrary/Abstract OCP/EmployeePayCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmployeeLibrary.Abstract_OCP
+{
+    public class EmployeePayCalculator
+    {
+        public EmployeePayCalculator()
+        {
+
+        }
+
+        public decimal CalculateAllPay(ICollection<Employee> employees)
+        {
+            return employees.Sum(x => x.CalculatePay());
+        }        
+    }
+}
---
+++ EmployeeLibrary/Implementation OCP/SalesEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeLibrary.Implementation_OCP
+{
+    public class SalesEmployee : SalaryEmployee
+    {
+        public decimal Sales { get; set; }
+
+        public decimal CommissionRate { get; set; }
+        public override decimal CalculatePay()
+        {
+            var regularPay = Salary / 26;
+
+            var bonusPay = CalculateBonus();
+
+            return regularPay + bonusPay;
+        }
+
+        public virtual decimal CalculateBonus()
+        {
+            return Sales * CommissionRate;
+        }
+    }
+}
